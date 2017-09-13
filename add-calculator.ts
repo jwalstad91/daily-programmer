1a4d025e42efca612d3c2fb5790085538fd58dc6
Jason Walstad <Jason.Walstad@experian.com>
2017-09-13 14:06:34 -0400

    expanding multiplation to account for all potential pos/neg outcomes

--- add-calculator.ts
+++ add-calculator.ts
@@ -2,9 +2,32 @@
     // cleanup
     // 0 cases
     // negative cases
+    // handling for non integral answer
 
-// setup. clean this up later to be nicer-looking
-let input = "72 / 12";
+let inputs = ['12 + 25',
+            '-30 + 100',
+            '100 - 30',
+            '100 - -30',
+            '-25 - 29',
+            '-41 - -10',
+            '9 * 3',
+            '9 * -4',
+            '-4 * 8',
+            '-12 * -9',
+            '100 / 2',
+            '75 / -3',
+            '-75 / 3',
+            '7 / 3',
+            '0 / 0',
+            '5 ^ 3',
+            '-5 ^ 3',
+            '-8 ^ 3',
+            '-1 ^ 1',
+            '1 ^ 1',
+            '0 ^ 5',
+            '5 ^ 0',
+            '10 ^ -3'];
+let input = "-12 * -9";
 let parsedInput = input.split(" ");
 let x = parseInt(parsedInput[0]);
 let y = parseInt(parsedInput[2]);
@@ -28,13 +51,21 @@ function subtract(x: number, y: number) {
 
 function multiply(x: number, y: number) {
     let result = 0;
+    let i = y;
 
-    // Check for case 0
-    if (x == 0 || y == 0) { return 0; }
+    // Resolve negative operand
+    if (y < 0) { i = negate(y); }
     
-    for (let i = 0; i < y; i++) {
+    // Check for case 0
+    if (x == 0 || y == 0) { return result; }
+
+    for (let j = 0; j < i; j++) {
         result += x;
     }
+
+    // Ensure proper sign is applied to result
+    if ((x < 0) && (y < 0)) { result = negate(result); }
+    else if (y < 0) { result = negate(result); }
 
     return result;
 }
